24ac2e531ba83ce6fbbb0bda6b78dd67f6e5a87e
Paulo Robson <p.robson085@gmail.com>
2026-09-09 15:18:44 -0300

    feat(userlist): prepara componente userlist

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,12 @@ import { store } from "./store";
 function App() {
   return (
     <Provider store={store}>
-      <main className="flex min-h-svh items-center justify-center bg-zinc-50 px-4 py-10">
+      <main className="flex min-h-svh items-start justify-center bg-zinc-50 px-4 py-10 gap-10">
         <section className="w-full max-w-md rounded-xl border border-zinc-200 bg-white p-8 shadow-sm">
           <FormHeader />
           <Form />
-          <UserList />
         </section>
+        <UserList />
       </main>
     </Provider>
   );
--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,3 +1,11 @@
 export function UserList() {
-  return <h1>Lista de Usuários</h1>;
+  return (
+    <section className="w-full max-w-md rounded-xl border border-zinc-200 bg-white p-8 shadow-sm p-8">
+      <header className="mb-8 text-left ">
+        <h1 className="text-2xl font-semibold text-zinc-800">
+          Lista de Usuários
+        </h1>
+      </header>
+    </section>
+  );
 }
